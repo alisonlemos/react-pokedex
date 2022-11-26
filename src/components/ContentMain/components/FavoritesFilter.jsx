850f579450ac12f818537df6a208a0af12ae1ab1
Alison <alisongarantizado@gmail.com>
2022-11-26 17:14:44 -0400

    Adicionando componente de filtro por favoritos

---
+++ src/components/ContentMain/components/FavoritesFilter.jsx
@@ -0,0 +1,30 @@
+import { Switch } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateFavoriteFilter } from '../../../store/pokedexSlice';
+import './FavoritesFilter.scss';
+
+function FavoritesFilter() {
+	const favoriteFilter = useSelector((state) => state.pokedex.favoriteFilter);
+	const dispatch = useDispatch();
+
+	const handleChange = (event) => {
+		const {
+			target: { checked },
+		} = event;
+		dispatch(updateFavoriteFilter(checked));
+	};
+
+	return (
+		<div className="favoriteFilter__container">
+			<h3>Filtrar Favoritos</h3>
+			<Switch
+				checked={favoriteFilter}
+				className="favoriteFilter__switch"
+				onChange={handleChange}
+				inputProps={{ 'aria-label': 'fitrar favoritos' }}
+			/>
+		</div>
+	);
+}
+
+export default FavoritesFilter;
---
+++ src/components/ContentMain/components/FavoritesFilter.test.js
@@ -0,0 +1,18 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../../testUtils';
+import userEvent from '@testing-library/user-event';
+
+import FavoritesFilter from './FavoritesFilter';
+
+describe('Componente de Filtro por favoritos', () => {
+	it('deve alternar o valor do state favoriteFilter entre true e false', async () => {
+		const { store } = renderWithProviders(<FavoritesFilter />);
+
+		const switchButton = screen.getByRole('checkbox', {
+			name: /fitrar favoritos/i,
+		});
+		expect(store.getState().pokedex.favoriteFilter).toBe(false);
+		await userEvent.click(switchButton);
+		expect(store.getState().pokedex.favoriteFilter).toEqual(true);
+	});
+});
