5a873f03b4e590f5ab6b5bc4b52ae47755b683ef
Alison <alisongarantizado@gmail.com>
2022-11-26 17:16:34 -0400

    Adicionando pagina principal da aplicacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { toggleLoading, updateCategoriesList } from './store/pokedexSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+	toggleLoading,
+	updateCategoriesList,
+	updatePokemonList,
+} from './store/pokedexSlice';
+import ContentHeader from './components/ContentHeader';
+import ContentMain from './components/ContentMain';
+import Header from './components/Header';
 import Loading from './common/Loading';
 import { getPokemonList } from './services';
 import './App.scss';
 
 function App() {
+	const searchKey = useSelector((state) => state.pokedex.searchKey);
+	const order = useSelector((state) => state.pokedex.order);
+	const categoriesList = useSelector((state) => state.pokedex.categoriesList);
+	const categoriesFilter = useSelector(
+		(state) => state.pokedex.categoriesFilter
+	);
+	const favoriteFilter = useSelector((state) => state.pokedex.favoriteFilter);
+	const favoriteList = useSelector((state) => state.pokedex.favoriteList);
+
 	const dispatch = useDispatch();
 	const [originalPokemonList, setOriginalPokemonList] = useState([]);
 
@@ -14,6 +30,32 @@ function App() {
 		const data = await getPokemonList();
 		setOriginalPokemonList(data);
 	}, [dispatch]);
+
+	const filterBySearchKey = useCallback((searchKey, pokemonList) => {
+		return pokemonList.filter(
+			(item) =>
+				item?.name.toLowerCase().includes(searchKey) ||
+				item?.national_number.toLowerCase().includes(searchKey)
+		);
+	}, []);
+
+	const filterByCategory = useCallback((filteredData, categoriesFilter) => {
+		if (!categoriesFilter.length) {
+			return filteredData;
+		}
+		return filteredData.filter((item) =>
+			item.type.some((type) => categoriesFilter.includes(type))
+		);
+	}, []);
+
+	const orderPokemonList = useCallback((order, pokemonList) => {
+		const compareFunction = (a, b) => {
+			return a[order.attribute] < b[order.attribute] ? -1 : 1;
+		};
+		return [...pokemonList].sort((a, b) =>
+			order.type === 'asc' ? compareFunction(a, b) : compareFunction(b, a)
+		);
+	}, []);
 
 	useEffect(() => {
 		fetchData();
@@ -29,8 +71,43 @@ function App() {
 		}
 	}, [originalPokemonList, dispatch]);
 
+	useEffect(() => {
+		if (categoriesList.length) {
+			dispatch(toggleLoading(true));
+
+			const data = favoriteFilter ? favoriteList : originalPokemonList;
+			const filteredData = filterBySearchKey(searchKey, data);
+			const filteredByCategoryData = filterByCategory(
+				filteredData,
+				categoriesFilter
+			);
+			const orderedData = orderPokemonList(order, filteredByCategoryData);
+			dispatch(updatePokemonList(orderedData));
+			setTimeout(() => {
+				dispatch(toggleLoading(false));
+			}, 1000);
+		}
+	}, [
+		order,
+		searchKey,
+		categoriesFilter,
+		categoriesList,
+		favoriteFilter,
+		favoriteList,
+		originalPokemonList,
+		dispatch,
+		filterBySearchKey,
+		filterByCategory,
+		orderPokemonList,
+	]);
+
 	return (
 		<div className="App">
+			<Header />
+			<main>
+				<ContentHeader />
+				<ContentMain />
+			</main>
 			<Loading />
 		</div>
 	);
